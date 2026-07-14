3c4f0560bc71621b2ca93cdb93600d0eb34dd866
bdpiwatcher <177953827+dmitrystarosta@users.noreply.github.com>
2026-07-14 15:17:46 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "ru.netstatus.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.3.3"
+        versionCode = 8
+        versionName = "0.4"
     }
 
     buildTypes {
